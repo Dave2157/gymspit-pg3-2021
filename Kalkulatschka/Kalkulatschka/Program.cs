89ed5dac0cc9ae0d4f4332d6ecdb479e55fb5c38
Dave2157 <66262306+Dave2157@users.noreply.github.com>
2021-09-23 14:56:42 +0200

    Add files via upload

---
+++ Kalkulatschka/Kalkulatschka/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Kalkulatschka
+{
+    class Program
+    {
+        static double calculate(double a, double b, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    return a / b;
+                default:
+                    return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            char op;
+            double a;
+            double b;
+
+            while (true)
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Zadej operaci");
+                    op = Console.ReadLine()[0];
+                    if (op == '0')
+                        return;
+                } while (!(op == '+' || op == '-' || op == '*' || op == '/'));
+
+                Console.WriteLine("Zadej Nummer 1");
+                if (!double.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.WriteLine("Keine Nummer!");
+                    Console.ReadLine();
+                    continue;
+                }
+                Console.WriteLine("Zadej Nummer 2");
+                if (!double.TryParse(Console.ReadLine(), out b))
+                {
+                    Console.WriteLine("Keine Nummer!");
+                    Console.ReadLine();
+                    continue;
+                }
+                if (op == '/' && b == 0)
+                {
+                    Console.WriteLine("Nulou nelze dělit!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Výsledek je: " + calculate(a, b, op));
+                Console.ReadLine();
+            }
+        }
+    }
+}
